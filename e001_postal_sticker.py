5eddb64b9bb7856cf2cbf44f0e87b417f9e1fb3e
Héctor Vega <linnk99@gmail.com>
2020-05-03 01:12:39 -0500

    Add files via upload

---
+++ e001_postal_sticker.py
@@ -0,0 +1,17 @@
+print('''
+Hector Vega
+Direccion 123
+Villas del Sauce
+12345
+La Paz, Baja California Sur
+Mexico
+
+ola ke ase
+''')
+
+
+""" print('Direccion 123')
+print('Villas del Sauce')
+print('12345')
+print('La Paz, Baja California Sur')
+print('Mexico') """
---
+++ e002_hello_name.py
@@ -0,0 +1,4 @@
+name = input('Input you name: ')
+age = input('Input your age: ')
+
+print(f'Hello, {name}. You are {age} years old')
